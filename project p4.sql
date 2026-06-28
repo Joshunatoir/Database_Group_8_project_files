b0d2c08c644ba21d13d9b968e17c40efb0734f7c
mjm29 <MarcJoseph.Millare@edu.sait.ca>
2026-06-27 23:49:29 -0600

    added drop tables

--- project p4.sql
+++ project p4.sql
@@ -1,7 +1,31 @@
 set ECHO ON
 spool c:/cprg250s/timber_project_p4.txt
 
-CREATE TABLE T_CUSTOMER (
+rem dropping child tables first
+rem drop timber_orderitems
+DROP TABLE timber_orderitems CASCADE CONSTRAINTS;
+rem drop timber_tax
+DROP TABLE timber_tax CASCADE CONSTRAINTS;
+rem drop timber_order
+DROP TABLE timber_order CASCADE CONSTRAINTS;
+rem drop timber_ship_rate
+DROP TABLE timber_ship_rate CASCADE CONSTRAINTS;
+rem drop timber_customer_product_review
+DROP TABLE timber_customer_product_review CASCADE CONSTRAINTS;
+rem drop timber_customer
+DROP TABLE timber_customer CASCADE CONSTRAINTS;
+rem drop timber_product_review
+DROP TABLE timber_product_review CASCADE CONSTRAINTS;
+rem drop timber_product_supplier
+DROP TABLE timber_product_supplier CASCADE CONSTRAINTS;
+rem drop timber_product
+DROP TABLE timber_product CASCADE CONSTRAINTS;
+rem drop timber_supplier
+DROP TABLE timber_supplier CASCADE CONSTRAINTS;
+rem drop timber_category
+DROP TABLE timber_category CASCADE CONSTRAINTS;
+
+CREATE TABLE timber_customer (
 	customer# NUMBER,
 	firstname VARCHAR2(50),
 	lastname VARCHAR2(50),
@@ -14,7 +38,7 @@ CREATE TABLE T_CUSTOMER (
 	isTimberMember NUMBER(1)
 );
 
-ALTER TABLE T_CUSTOMER ADD(
+ALTER TABLE timber_customer ADD(
 	CONSTRAINT t_customer_cust_num_pk PRIMARY KEY (customer#),
 	CONSTRAINT t_customer_phnum_ck CHECK (REGEXP_LIKE (phoneNumber,'^\d{3}\.\d{3}\.\d{4}$')),
 	CONSTRAINT t_customer_prov_ck CHECK (prov IN ('ab','bc','sk','mb','on','qc','pe','nl','nt','yt','ns','nb','nu')),
@@ -24,7 +48,7 @@ ALTER TABLE T_CUSTOMER ADD(
 	CONSTRAINT t_customer_timember CHECK(isTimberMember IN(0, 1))
 );
 	
-ALTER TABLE T_CUSTOMER MODIFY(
+ALTER TABLE timber_customer MODIFY(
 	firstname, NOT NULL,
 	lastname, NOT NULL,
 	phoneNumber, NOT NULL,
@@ -35,20 +59,20 @@ ALTER TABLE T_CUSTOMER MODIFY(
 	customerEmail,NOT NULL	
 );
 
-CREATE TABLE T_TAX(
+CREATE TABLE timber_tax(
 	prov CHAR(2),
 	prov_hst_rate NUMBER(4,3),
 	gst_rate NUMBER(4,3),
 	pst_rate NUMBER(4,3)
 );
 
-CREATE TABLE T_SHIPPING_RATE(
+CREATE TABLE timber_ship_rate(
 	shiprate# NUMBER,
 	min_weight NUMBER(6,2),
 	max_weight NUMBER(6,2),
 	shippng_amount NUMBER(6,2)
 );
-CREATE TABLE T_ORDER (
+CREATE TABLE timber_order (
 	order# NUMBER CONSTRAINT t_order_ordnum_pk PRIMARY KEY,
 	customer# NUMBER CONSTRAINT t_order_custnum_fk FOREIGN KEY,
 	orderDate DATE CONSTRAINT t_order_orderDate_nn NOT NULL,
